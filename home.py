3bff482fb9f482bd0eeb5837c99e957c0d72c5ea
Kinal Patel <136503460+kinal1999@users.noreply.github.com>
2023-08-01 11:43:17 -0500

    Add files via upload

---
+++ home.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.markdown("# Center of Mass ⚛️")
+st.divider()
+    
+st.markdown("""Calculates the weighted center of mass for structures in a PDB file. All atoms in the PDB file are included in the calculation.""")
+
+st.markdown("""
+- **Home:** You are here!
+- **Protein:** Center of Mass for Proteins
+- **Ligand:** Center of Mass for Ligands
+""")
+st.divider()
+st.markdown("""
+    [**GitHub**](https://github.com/rasbt/protein-science/tree/master) 💻
+    """)
+st.text("")
+st.markdown("""App Created by [Health Universe](https://www.healthuniverse.com) 🚀
+            (Kinal Patel and Mitchell Parker)""")
---
+++ pages/2_Ligand.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import json
+import os
+import pandas as pd
+import io
+import subprocess
+import time
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "CenterOfMass"))
+
+from center_of_mass import center_of_mass
+
+st.markdown("## Center of Mass - Ligand")
+st.divider()
+
+# File uploader
+pdb_file = st.file_uploader(
+    "Upload",
+    type="pdb",
+    help="**Input:** Protein PDB \n\n**Output:** Center of Mass of Ligand"
+)
+
+# File Processing
+if pdb_file is not None:
+    run_button = st.button("Run", help="Calculating Center of Mass of Ligand")
+    if run_button:
+        with st.spinner("Running..."):
+            time.sleep(2)
+            # Save files
+            pdb_path = "./protein.pdb"
+            
+            # Read file values
+            pdb_data = pdb_file.getvalue()
+            
+            with open(pdb_path, "wb") as f:
+                f.write(pdb_data)
+            
+            i = "HETATM"
+                
+            # Run script via subprocess
+            command = (
+                f"python center_of_mass.py protein.pdb "
+                f"-i {i}"
+            )
+            
+            result = subprocess.run(command, shell=True, capture_output=True, text=True)
+
+            # Display the output
+            st.divider()
+            st.subheader("Output")
+            st.write("Center of Mass for the ligand is the following:")
+            st.code(result.stdout)
+            
+            
+
